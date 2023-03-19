2e3b780054370d3d724f6b500eca2c0dc4dc92da
Abdul Waheed <ab.waheedkashmiri@gmail.com>
2023-03-19 21:56:02 +0500

    api redeployed and endpoint added.

--- src/core/endpoints.js
+++ src/core/endpoints.js
@@ -1,4 +1,4 @@
-const API_PREFIX = 'http://localhost:5000/api'
+const API_PREFIX = 'https://exercise-tracker-api-production.up.railway.app/api'
 
 export const userEndpoint = `${API_PREFIX}/users`
 export const exerciseEndpoint = `${API_PREFIX}/exercises`
